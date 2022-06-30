f005404b0aee71f2d37d7158b4a5b3adb71b957d
colinluetzelschwab <colin.luetzelschwab@gmail.com>
2022-06-30 15:48:26 +0200

    added chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,65 @@
 import * as React from 'react';
-import {Button, Text, TextInput, View, StyleSheet} from 'react-native';
+import {TextInput, View, StyleSheet, TouchableOpacity, Image, Text, ScrollView} from 'react-native';
 
 export default function Chat() {
     return (
-        <View style={{flex: 1, alignItems: 'center'}}>
-            <TextInput
-                style={styles.input}
-                placeholder='Type something...'/>
+        <View style={styles.chatContainer}>
+            <ScrollView style={styles.chatContainerBubbles}>
+                <Text>jaööp</Text>
+                <Text>jaööp</Text>
+            </ScrollView>
+            <View style={styles.chatContainerInput}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Type something...'>
+                </TextInput>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>send</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    chatContainer: {
+        height: "100%",
+        flexDirection: "column",
+        backgroundColor: "red",
+        display: "flex",
+    },
+    chatContainerBubbles: {
+        backgroundColor: "green",
+        flexGrow: 55,
+        flexBasis: "auto",
+    },
+    chatContainerInput: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "darkgrey",
+        maxWidth: "100%",
+        flexGrow: 5,
+        //kritisch
+        paddingBottom: 11,
+    },
     input: {
-        height: 40,
         margin: 12,
         borderWidth: 1,
+        borderRadius: 15,
         padding: 10,
-
+        flexGrow: 6,
+    },
+    button: {
+        margin: 12,
+        height: 40,
+        width: 60,
+        backgroundColor: '#2196F3',
+        borderRadius: 15,
+        flexGrow: 1,
+    },
+    buttonText: {
+        textAlign: 'center',
+        padding: 11,
+        color: 'white'
     }
 })
